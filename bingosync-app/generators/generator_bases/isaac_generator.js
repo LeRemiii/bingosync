94b46d7e797e7e8732f2d2e498a9b926b6c07fe2
LeRemiii <42846930+LeRemiii@users.noreply.github.com>
2018-09-06 19:15:00 +0200

    Bug fix in the grid shape

--- bingosync-app/generators/generator_bases/isaac_generator.js
+++ bingosync-app/generators/generator_bases/isaac_generator.js
@@ -21,7 +21,7 @@ bingoGenerator = function (bingoList, opts) {
     Math.seedrandom(SEED);
     var bingoBoard = [];
     var easyTiles = [3,4,7,10,11,15,17,19,21,23];//shaping the grid
-    var mediumTiles = [1,5,8,9,12,14,16,18,22,25];
+    var mediumTiles = [2,5,8,9,12,14,16,18,22,25];
     var hardTiles = [1,6,20,24];
     var extraHardTiles = [13];
 
